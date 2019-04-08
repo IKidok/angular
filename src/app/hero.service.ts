6112777ba5c09f974ec08e93f59f7bf12e55b8b0
Ivan Shmatko <vanya-shmatko@yandex.ru>
2019-04-09 00:10:17 +0300

    Routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,4 +14,9 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fethched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
