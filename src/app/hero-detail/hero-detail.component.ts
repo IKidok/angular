9a38c44eda0e3145b22b43aa0e063c7f4ce4035b
Ivan Shmatko <vanya-shmatko@yandex.ru>
2019-04-09 01:41:28 +0300

    HTTP | Ended

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,3 @@
-import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -32,6 +31,11 @@ export class HeroDetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 
 }
